7640d8eeaf542ed7227c4e9c8a1d6dfd399a907a
toddpeltzer <toddpeltzer14@gmail.com>
2022-10-03 15:44:36 -0700

    blank questions

--- fundamentals.js
+++ fundamentals.js
@@ -1,39 +1,24 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-// var foods = ["apple",  "orange", "plum"]
- let foods = ["pizza", "burger", "ice cream"]
-console.log(foods)
+
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-// var last = foods[2]
-let last = foods[2]
-console.log(last)
+
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-// var favoriteFoods = []
-let favoriteFoods = []
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-// for (i=0; i<foods.length; i++) {
-//     favoriteFoods.push(foods[i])
-// }
-
-for (i=0; i<foods.length; i++)
-    let favoriteFoods = push().foods[i]
-    console.log(favoriteFoods)
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs
 // Type your solution immediately below this line:
-// var instructor = {name: 'Tom', age: 40, subject: 'history'}
 
 
 // #6: Add a `has-office-hours` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
-// instructor['has-office-hours'] = true
 
 // if you are confident up to this part, scaffold up an HTML file and use basic js to render your favorite foods on screen, or map out your instructors, 
 // and set up a button that filters out the ones who do not have office hours
